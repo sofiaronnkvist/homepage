2534314eb9e9415a2f503a83f964419e8c3e8fbd
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2024-02-15 22:50:47 +0100

    Add animated circles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import favicon from "./icon.svg"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div>
-        <h1>Under construction</h1>
+      <div className="w-full flex gap-10 flex-col">
+        <h1 className="text-2xl text-center">Under construction...</h1>
+        <div className="w-full flex align-middle justify-center">
+          <Image alt="circle" src={favicon} width={200} className="animate-pulse"/>
+        </div>
       </div>
 
     </main>
