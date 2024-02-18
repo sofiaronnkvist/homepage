eea7c8fe4f0c9de8f95496b0128fdda509645b0d
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2024-02-18 19:11:52 +0100

    Add nav

---
+++ src/app/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import linkImage from "./linkImage.svg"
+import Image from "next/image";
+
+export default function Navbar() {
+    return (
+        <nav className="flex w-full fixed nav justify-center gap-4 font-poppins text-lg top-3">
+            <Link className="hover:animate-pulse font-poppins" href="#header">Index</Link>
+            <div className="flex gap-1">
+                <Link className="hover:animate-pulse font-libreCaslonText" target="_blank" href="https://github.com/sofiaronnkvist">Projects</Link>
+                <Image src={linkImage} alt="Link" width={12} height={12}/>
+            </div>
+            <Link className="hover:animate-pulse font-poppins" href="#contact">Contact</Link>
+        </nav>
+    );
+}
