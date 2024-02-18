1284cdddcd1d4653f0607f3d6c5475dfc75f6c05
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2024-02-18 19:12:37 +0100

    Add header sections

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="p-8 flex flex-row">
+          <h1 className="text-4xl lg:text-8xl font-libreCaslonText">Welcome</h1>
+        </div>
+    );
+}
